f1c98ce764a7bc2bea156fca24c4f11ac3c86661
moses ubah <39748749+mohzys23@users.noreply.github.com>
2020-10-05 16:51:43 +0000

    header css formatting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Introduction</h1>
-    <p>React will be fun.</p>
-    </div>
+    <p></p>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import './Header.css';
+import download from '../download.png';
 
 function Header () {
     return (
       <nav>
-          <ul>
+          <div>
+<img src={download} alt="code icon" class="top-img"/>
+          </div>
+          <div>
+               <ul>
               <li><a href="#">Home</a></li>
               <li><a href="#">Contact</a></li>
               <li><a href="#">About-us</a></li>
+              <li><a href="#">Privacy Policy</a></li>
           </ul>
+          </div>
+         
       </nav>
     );
 }
