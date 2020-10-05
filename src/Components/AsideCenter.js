dad7f0f4f7c975d336d13b71c313bb6cf5fee636
moses ubah <39748749+mohzys23@users.noreply.github.com>
2020-10-05 20:23:19 +0000

    css formatting

--- src/Components/AsideCenter.js
+++ src/Components/AsideCenter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './AsideCenter.css';
 
 function AsideCenter() {
     return (
@@ -7,10 +7,10 @@ function AsideCenter() {
 <table>
     <th>Items</th><th>Brand</th><th>Color</th><th>Size</th>
     <tr><td>#101</td><td>D&G</td><td>Red</td><td>45</td></tr>
-        <tr><td>#101</td><td>D&G</td><td>Red</td><td>45</td></tr>
-            <tr><td>#101</td><td>D&G</td><td>Red</td><td>45</td></tr>
-                <tr><td>#101</td><td>D&G</td><td>Red</td><td>45</td></tr>
-
+        <tr><td>#102</td><td>GUCCI</td><td>Blue & Black</td><td>12</td></tr>
+            <tr><td>#103</td><td>ZARA</td><td>Yellow</td><td>6</td></tr>
+                <tr><td>#104</td><td>CALVIN KLEIN</td><td>Blue</td><td>10</td></tr>
+<tr><td>#105</td><td>LOUIS VUITON</td><td>Black</td><td>20</td></tr>
 
 
 </table>
