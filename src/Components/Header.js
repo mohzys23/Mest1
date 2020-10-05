135ed91b24866370ee0b98613738971d1d184dab
moses ubah <39748749+mohzys23@users.noreply.github.com>
2020-10-05 13:33:52 +0000

    created header component

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header () {
+    return (
+      <nav>
+          <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">About-us</a></li>
+          </ul>
+      </nav>
+    );
+}
+
+export default Header;
